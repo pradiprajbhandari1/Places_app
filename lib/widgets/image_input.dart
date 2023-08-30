1b1c822111179f353ed43b55f9692a7a2879ef2a
pradip <rajbhandaripradip00@gmail.com>
2023-08-30 11:32:27 +1000

    path location updated

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 class ImageInput extends StatefulWidget {
   const ImageInput({Key key}) : super(key: key);
 
@@ -12,13 +14,22 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
  File _storedImage;
- final ImagePicker _picker = ImagePicker();
-  Future<void> _takePicture() async {
 
-    final imageFile = await _picker.pickImage(
+  Future<void> _takePicture() async {
+    final  picker = ImagePicker();
+    final imageFile = await picker.pickImage(
  source:ImageSource.camera,
  maxWidth:600,
  );
+    setState(() {
+      _storedImage =File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+   final fileName = path.basename(imageFile.path);
+    final savedImage = await _storedImage.copy('${appDir.path}/$fileName');
+
+
+
  }
 
   @override
